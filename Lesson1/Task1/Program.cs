fcbd4e987bb5f042d99a85b513028368445ef820
unknown <bogdan.volk0807@gmail.com>
2024-03-13 21:36:37 +0200

    add lesson1 tip

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,13 +1,22 @@
-﻿// int i1 = 5;
-// int i2 = 6;
-// int sum = i1 + i2;
-// Console.WriteLine(sum);
+﻿
 
-long i1 = 6;
-long sum2 = i1 + i1;
-Console.WriteLine(sum2);
+int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+// long i1 = 6;
+// long sum2 = i1 + i1;
+// Console.WriteLine(sum2);
 
 double d1 = 5.5;
 double sum3 = d1 + i1;
 Console.WriteLine(sum3);
+
+int qoutient1 = i1 / i2;
+Console.WriteLine(qoutient1);
+
+d1 = 6;
+double  qoutient2 = i1 / d1;
+Console.WriteLine( qoutient2);
 
