b270a4383e98968724150bbc74a7d20011e9c873
unknown <bogdan.volk0807@gmail.com>
2024-03-13 21:19:54 +0200

    add lesson;task1

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// int i1 = 5;
+// int i2 = 6;
+// int sum = i1 + i2;
+// Console.WriteLine(sum);
+
+long i1 = 6;
+long sum2 = i1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
